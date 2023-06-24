a68e6a79ba094e18300b416e3388c21feb68a6f5
Yuriy Chamkoriyski <chamkoriyski@gmail.com>
2023-06-24 21:28:55 +0200

    Create the Post model and controller

--- server/index.js
+++ server/index.js
@@ -3,14 +3,20 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
+import postRoutes from './routes/posts.js';
+
 const app = express();
+
+app.use('/posts', postRoutes);
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
 
-const CONNECTION_URL = process.env.CONNECTION_URL;
+// const CONNECTION_URL = process.env.CONNECTION_URL;
+const CONNECTION_URL = 'mongodb+srv://chamkoriyski:BWMcpLBmmg9J9ASB@cluster0.f0m95ar.mongodb.net/?retryWrites=true&w=majority';
+
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })

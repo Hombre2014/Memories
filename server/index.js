71465d0b713ec330d31bfb958e6dd954ccafe739
Yuriy Chamkoriyski <chamkoriyski@gmail.com>
2023-06-24 21:55:12 +0200

    Add dotenv

--- server/index.js
+++ server/index.js
@@ -5,6 +5,9 @@ import cors from 'cors';
 
 import postRoutes from './routes/posts.js';
 
+import dotenv from 'dotenv';
+dotenv.config({ path: './.env' });
+
 const app = express();
 
 app.use('/posts', postRoutes);
@@ -13,13 +16,11 @@ app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
-
-// const CONNECTION_URL = process.env.CONNECTION_URL;
-const CONNECTION_URL = 'mongodb+srv://chamkoriyski:BWMcpLBmmg9J9ASB@cluster0.f0m95ar.mongodb.net/?retryWrites=true&w=majority';
+const url = process.env.CONNECTION_URL;
 
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
 

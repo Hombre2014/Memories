f8d77132a12d8858754c14e9656d07c3f5e26709
Yuriy Chamkoriyski <chamkoriyski@gmail.com>
2023-08-02 12:11:34 +0200

    Implemented Google and normal signin.

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 
 import postRoutes from './routes/posts.js';
+import userRoutes from './routes/user.js';
 
 import dotenv from 'dotenv';
 dotenv.config({ path: './.env' });
@@ -14,6 +15,7 @@ app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 app.use('/posts', postRoutes);
+app.use('/user', userRoutes);
 
 const url = process.env.CONNECTION_URL;
 

42c35c56d44788ad019d909bbd696becbba8a834
Yuriy Chamkoriyski <chamkoriyski@gmail.com>
2023-08-03 21:47:22 +0200

    Add post details and fetch a single post.

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,12 +1,13 @@
 import express from 'express';
 
-import { getPostBySearch, getPosts, createPost, updatePost, deletePost, likePost } from '../controllers/posts.js';
+import { getPostBySearch, getPost, getPosts, createPost, updatePost, deletePost, likePost } from '../controllers/posts.js';
 import auth from '../middleware/auth.js';
 
 const router = express.Router();
 
 router.get('/search', getPostBySearch);
 router.get('/', getPosts);
+router.get('/:id', getPost);
 router.post('/', auth, createPost);
 router.patch('/:id', auth, updatePost);
 router.delete('/:id', auth, deletePost);
